552cc2e7288adf1d64daef2e3a63067dc315a835
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-01 18:50:40 -0300

    + Finished signup validation + Inserted better forms design + Input components and functions better divided

---
+++ src/components/TextFieldModifiedPassword.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { TextField, IconButton } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { setOnBlur, setFieldValue } from "./TextFieldModified";
+
+export default function TextFieldModifiedPassword({ nameOfVar, nameOfFunc }) {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <TextField
+      helperText={
+        nameOfVar.blur === "true" &&
+        nameOfVar.value === "" &&
+        "Please, insert a password"
+      }
+      error={nameOfVar.blur === "true" && nameOfVar.value === "" && true}
+      id="outlined-basic"
+      label="Password"
+      variant="outlined"
+      onBlur={() => setOnBlur(nameOfFunc)}
+      onChange={(e) => {
+        const value = e.target.value;
+        setFieldValue(nameOfFunc, value);
+      }}
+      type={showPassword ? "Text" : "Password"}
+      margin={"normal"}
+      FormHelperTextProps={{
+        style: {
+          fontSize: "12px",
+        },
+      }}
+      InputProps={{
+        endAdornment: (
+          <IconButton
+            style={{ marginRight: "-.7rem" }}
+            aria-label="toggle password visibility"
+            onClick={() => setShowPassword((prevState) => !prevState)}
+            // onMouseDown={handleMouseDownPassword}
+            edge="end"
+          >
+            {showPassword ? (
+              <Visibility
+                style={{
+                  fontSize: "23px",
+                  color: "rgba(107, 110, 202, 0.8)",
+                }}
+              />
+            ) : (
+              <VisibilityOff
+                style={{
+                  fontSize: "23px",
+                  color: "rgba(107, 110, 202, 0.8)",
+                }}
+              />
+            )}
+          </IconButton>
+        ),
+      }}
+    />
+  );
+}
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -6,15 +6,29 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { postSignup } from "../../asyncActions/postSignup";
 import { TextField, IconButton } from "@material-ui/core";
-import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { Visibility, VisibilityOff, AlternateEmail } from "@material-ui/icons";
+import TextFieldModified from "../TextFieldModified";
+import TextFieldModifiedPassword from "../TextFieldModifiedPassword";
 
 function Signup() {
-  const [username, setUsername] = useState({ blur: "false", value: "" });
-  const [email, setEmail] = useState({ blur: "false", value: "" });
-  const [password, setPassword] = useState({ blur: "false", value: "" });
-  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState({
+    focused: "false",
+    blur: "false",
+    value: "",
+  });
+  const [email, setEmail] = useState({
+    focused: "false",
+    blur: "false",
+    value: "",
+  });
+  const [password, setPassword] = useState({
+    focused: "false",
+    blur: "false",
+    value: "",
+  });
 
   const dispatch = useDispatch();
+
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <Presentation
@@ -36,102 +50,21 @@ function Signup() {
           }}
           className="App__form"
         >
-          <TextField
-            error
-            id="outlined-basic"
+          <TextFieldModified
+            nameOfVar={username}
+            nameOfFunc={setUsername}
             label="Username"
-            variant="outlined"
           />
-          <TextField
-            id="outlined-basic"
+          <TextFieldModified
+            nameOfVar={email}
+            nameOfFunc={setEmail}
             label="E-mail"
-            variant="outlined"
-            onChange={(e) => console.log(e.target.value)}
           />
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            type={showPassword ? "Text" : "Password"}
-            InputProps={{
-              endAdornment: (
-                <IconButton
-                  style={{ marginRight: "1rem" }}
-                  aria-label="toggle password visibility"
-                  onClick={() => setShowPassword((prevState) => !prevState)}
-                  // onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? (
-                    <Visibility style={{ fontSize: "23px" }} />
-                  ) : (
-                    <VisibilityOff style={{ fontSize: "23px" }} />
-                  )}
-                </IconButton>
-              ),
-            }}
+          <TextFieldModifiedPassword
+            nameOfVar={password}
+            nameOfFunc={setPassword}
           />
-          {/* <label>
-            Username
-            <input
-              type="text"
-              value={username.value}
-              style={{
-                borderColor:
-                  username.blur === "true" && username.value === "" && "red",
-              }}
-              onBlur={() => {
-                setUsername((prevState) => ({ ...prevState, blur: "true" }));
-              }}
-              onChange={(e) => {
-                let tv = e.target.value;
-                setUsername((prevState) => ({
-                  ...prevState,
-                  value: tv,
-                }));
-              }}
-            />
-          </label>
-          <label>
-            E-mail
-            <input
-              type="email"
-              style={{
-                borderColor:
-                  email.blur === "true" && email.value === "" && "red",
-              }}
-              onBlur={() => {
-                setEmail((prevState) => ({ ...prevState, blur: "true" }));
-              }}
-              onChange={(e) => {
-                let tv = e.target.value;
-                setEmail((prevState) => ({
-                  ...prevState,
-                  value: tv,
-                }));
-              }}
-            />
-          </label> */}
-          {/* <label>
-            Senha
-            <input
-              type="password"
-              style={{
-                borderColor:
-                  password.blur === "true" && password.value === "" && "red",
-              }}
-              onBlur={() => {
-                setPassword((prevState) => ({ ...prevState, blur: "true" }));
-              }}
-              onChange={(e) => {
-                let tv = e.target.value;
-                setPassword((prevState) => ({
-                  ...prevState,
-                  value: tv,
-                }));
-              }}
-            />
-          </label> */}
+
           <button
             className="App__form--button"
             style={{ marginTop: "2.8rem", width: "100%" }}
