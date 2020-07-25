4fa72f91dd5efbb700dde5222503fabbc8d9cfe1
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-24 23:24:08 -0300

    Clean unused comments in app component

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../../routes';
-// import Error from "./components/errors/Error";
-// import {} from "./slices/rootSlice";
 
 function App() {
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
@@ -14,13 +12,12 @@ function App() {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-  // const dataIsFetched = useSelector((d) => d.rootReducer.dataIsFetched);
+
   return (
     <BrowserRouter>
       <div className="App" style={{ height: `${innerHeight}px` }}>
         <Routes />
         <div className="info">{`iH: ${window.innerHeight}  iW: ${window.innerWidth} `}</div>
-        {/* {dataIsFetched === "error" && <Error />} */}
       </div>
     </BrowserRouter>
   );
