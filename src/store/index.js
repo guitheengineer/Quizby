585126a4f319a1d199d8839e705e404ce60e9ed3
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-24 22:49:38 -0300

    Remove unused Reducer from configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../slices/rootSlice';
 import formReducer from '../slices/formSlice';
 import quizzesReducer from '../slices/quizzesSlice';
 
 export default configureStore({
   reducer: {
-    rootReducer,
     formReducer,
     quizzesReducer,
   },
