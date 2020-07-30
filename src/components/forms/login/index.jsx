45a3515bf389fbe95b947ffe468bc39eb1b0d7cb
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-30 20:56:25 -0300

    Login general improvements Add a component to handle login errors Add a loader to indicate that the login is happening

--- src/components/forms/login/index.jsx
+++ src/components/forms/login/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ClipLoader from 'react-spinners/ClipLoader';
+
 import Presentation from '../Presentation';
 import { postLogin } from '../../../asyncActions';
 import BackgroundContainer from '../../backgroundcontainer';
@@ -11,6 +13,8 @@ import { onSubmitForm } from '../../../slices/formSlice';
 
 export default function Login() {
   const dispatch = useDispatch();
+  const { loginError, loginState } = useSelector((data) => data.formReducer);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Presentation
@@ -21,25 +25,53 @@ export default function Login() {
       <BackgroundContainer mgTop="3.2rem" minHeight="35.7rem">
         <form
           onSubmit={(e) => {
-            dispatch(onSubmitForm());
+            const email = e.target[0].value;
+            const password = e.target[2].value;
             dispatch(
-              postLogin({
-                email: e.target[0].value,
-                password: e.target[2].value,
+              onSubmitForm({
+                email,
+                password,
               })
             );
+            const regexEmailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (regexEmailValidator.test(email) && password.length >= 8) {
+              dispatch(
+                postLogin({
+                  email,
+                  password,
+                })
+              );
+            }
+
             e.preventDefault();
           }}
           className="App__form"
         >
+          {loginError && (
+            <p className="App__form--loginerror">{loginError.errorDes}</p>
+          )}
           <TextFieldModifiedEmail />
           <TextFieldModifiedPassword />
           <button
             className="App__form--button"
             type="submit"
-            style={{ marginTop: '3.5rem', width: '100%' }}
+            style={{
+              marginTop: '3.5rem',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
           >
-            Login
+            <span>Login</span>
+            <ClipLoader
+              loading={loginState === 'loading'}
+              size="14"
+              color="white"
+              css={`
+                margin-left: 5px;
+              `}
+            />
           </button>
         </form>
         <div className="App__registerAdvice">
