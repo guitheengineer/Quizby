b45ee6390993c883930b22b6402d58553732a2fe
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-30 19:51:50 -0300

    Remove some unused console.log statements

--- controllers/quiz/currentQuiz.js
+++ controllers/quiz/currentQuiz.js
@@ -2,9 +2,7 @@ const { quizModel } = require('../../models');
 
 module.exports = async (req, res) => {
   const id = req.params.id.substring(1);
-  console.log(id);
   const quiz = await quizModel.findById(id);
-  console.log(quiz);
   res.status(200).json({
     status: 'success',
     quiz,
