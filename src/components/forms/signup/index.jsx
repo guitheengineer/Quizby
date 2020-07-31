6ee86bcbace6eb379dee25a018b88136642e341d
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-07-30 21:25:49 -0300

    Add loading indicator to signup component

--- src/components/forms/signup/index.jsx
+++ src/components/forms/signup/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import ClipLoader from 'react-spinners/ClipLoader';
 import Presentation from '../Presentation';
 import BackgroundContainer from '../../backgroundcontainer';
-import { postSignup, checkIfUserExists } from '../../../asyncActions';
+import {
+  postSignup,
+  checkIfUserExists,
+  checkIfEmailExists,
+} from '../../../asyncActions';
 import TextFieldModifiedUsername from '../../textfields/TextFieldModifiedUsername';
 import TextFieldModifiedPassword from '../../textfields/TextFieldModifiedPassword';
 import TextFieldModifiedEmail from '../../textfields/TextFieldModifiedEmail';
@@ -10,6 +15,7 @@ import { onSubmitForm } from '../../../slices/formSlice';
 
 function Signup() {
   const dispatch = useDispatch();
+  const signupState = useSelector((data) => data.formReducer.signupState);
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Presentation
@@ -20,20 +26,33 @@ function Signup() {
       <BackgroundContainer mgTop="2.7rem" minHeight="38.7rem">
         <form
           onSubmit={(e) => {
-            console.log(
-              e.target[0].value,
-              e.target[2].value,
-              e.target[4].value
-            );
-            dispatch(onSubmitForm());
-            dispatch(checkIfUserExists(e.target[0].value));
+            const username = e.target[0].value;
+            const email = e.target[2].value;
+            const password = e.target[4].value;
             dispatch(
-              postSignup({
-                username: e.target[0].value,
-                email: e.target[2].value,
-                password: e.target[4].value,
+              onSubmitForm({
+                username,
+                email,
+                password,
               })
             );
+            dispatch(checkIfUserExists(username));
+            dispatch(checkIfEmailExists(email));
+            const regexEmailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const regexUsernameValidator = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+            if (
+              regexUsernameValidator.test(username) &&
+              regexEmailValidator.test(email) &&
+              password.length >= 8
+            ) {
+              dispatch(
+                postSignup({
+                  username,
+                  email,
+                  password,
+                })
+              );
+            }
             e.preventDefault();
           }}
           className="App__form"
@@ -48,10 +67,24 @@ function Signup() {
             //   password.value.length < 8
             // }
             className="App__form--button"
-            style={{ marginTop: '2.8rem', width: '100%' }}
+            style={{
+              marginTop: '2.8rem',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
             type="submit"
           >
-            Registrar
+            <span>Registrar</span>
+            <ClipLoader
+              loading={signupState === 'loading'}
+              size="14px"
+              color="white"
+              css={`
+                margin-left: 5px;
+              `}
+            />
           </button>
         </form>
       </BackgroundContainer>
