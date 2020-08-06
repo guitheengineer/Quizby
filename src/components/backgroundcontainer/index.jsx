940f4d6cb449e109a7b5f87034fe9e4230a4fd44
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-06 16:30:52 -0300

    Reduce code repetition and set new propTypes to backgroundcontainer

--- src/components/backgroundcontainer/index.jsx
+++ src/components/backgroundcontainer/index.jsx
@@ -3,9 +3,28 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import Menu from '../menu';
 
-function BackgroundContainer({ mgTop, minHeight, children, justifyContent }) {
+function BackgroundContainer({
+  mgTop,
+  minHeight,
+  children,
+  justifyContent,
+  overflow,
+  alignItems,
+}) {
   const { menuIsActive } = useSelector((data) => data.generalReducer);
-  console.log(menuIsActive, ' < menu');
+
+  const commonStyles = {
+    position: 'relative',
+    backgroundColor: '#F8FAF7',
+    borderRadius: '20px 20px 0px 0px',
+    width: '100',
+    zIndex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    transition: 'all 2s',
+    alignItems,
+    justifyContent,
+  };
   return (
     <>
       {menuIsActive && <Menu />}
@@ -13,35 +32,20 @@ function BackgroundContainer({ mgTop, minHeight, children, justifyContent }) {
         style={
           menuIsActive
             ? {
-                position: 'relative',
-                backgroundColor: '#F8FAF7',
-                borderRadius: '20px 20px 0px 0px',
-                width: '100%',
-                zIndex: 1,
-                display: 'flex',
-                flexDirection: 'column',
+                ...commonStyles,
+                overflow: 'hidden',
                 marginTop: 'auto',
                 height: '4rem',
-                justifyContent,
-                transition: 'all 2s',
-                overflowY: 'hidden',
               }
             : {
-                position: 'relative',
-                backgroundColor: '#F8FAF7',
-                borderRadius: '20px 20px 0px 0px',
-                width: '100%',
+                ...commonStyles,
+                overflow,
                 flex: '1 1 100%',
-                zIndex: 1,
-                display: 'flex',
-                flexDirection: 'column',
                 marginTop: mgTop,
                 minHeight,
-                justifyContent,
               }
         }
       >
-        {/* {!menuIsActive && children} */}
         {Children.map(children, (child) =>
           cloneElement(child, {
             className: `${child.props.className} ${
@@ -57,13 +61,17 @@ function BackgroundContainer({ mgTop, minHeight, children, justifyContent }) {
 BackgroundContainer.propTypes = {
   minHeight: PropTypes.string,
   justifyContent: PropTypes.string,
-  children: PropTypes.object.isRequired,
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
   mgTop: PropTypes.string,
+  overflow: PropTypes.string,
+  alignItems: PropTypes.string,
 };
 
 BackgroundContainer.defaultProps = {
   mgTop: '0px',
   minHeight: '0px',
   justifyContent: 'center',
+  overflow: 'hidden',
+  alignItems: 'normal',
 };
 export default BackgroundContainer;
