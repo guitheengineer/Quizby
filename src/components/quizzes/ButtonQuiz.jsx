4022285b0f5e7177d8fd30880af5f83847df4b9a
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-10 15:09:07 -0300

    Added functional background quizzes image with styles

--- src/components/quizzes/ButtonQuiz.jsx
+++ src/components/quizzes/ButtonQuiz.jsx
@@ -12,8 +12,16 @@ function ButtonQuiz({ quiz }) {
     history.push(`/quizzes/show/${quiz._id}`);
     dispatch(setQuery(''));
   }
+  function getQuizBackground() {
+    if (quiz.image) {
+      return {
+        backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.27), rgba(0, 0, 0, 0.27)), url('data:${quiz.image.contentType};base64,${quiz.image.data}')`,
+      };
+    }
+  }
+
   return (
-    <button type="button" onClick={quizClicked}>
+    <button style={getQuizBackground()} type="button" onClick={quizClicked}>
       <span>{quiz.name}</span>
     </button>
   );
@@ -23,6 +31,7 @@ ButtonQuiz.propTypes = {
   quiz: PropTypes.shape({
     name: PropTypes.string,
     _id: PropTypes.string,
+    image: PropTypes.object,
   }).isRequired,
 };
 
