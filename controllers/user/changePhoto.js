6ccacf29349925a4d4bace0c0bf37c2bb440c7e7
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-10 01:51:12 -0300

    Add functional but hardcoded url image change system

--- controllers/user/changePhoto.js
+++ controllers/user/changePhoto.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const { userModel } = require('../../models');
 
-const pathImg = '';
+// maybe I need to check if the id is the same as the user is
+const pathImg = '/home/guilhermesnd/Área de Trabalho/download.jpeg';
 module.exports = async (req, res) => {
   const { id } = req.body;
+
   const data = fs.readFileSync(pathImg);
-  const contentType = 'image/png';
+  const base64image = Buffer.from(data).toString('base64');
+
+  const contentType = `image/${pathImg.split('.').pop()}`;
+
   const filter = { _id: id };
-  const update = { avatar: { data, contentType } };
+  const update = { avatar: { base64image, contentType } };
 
   const doc = await userModel.findOneAndUpdate(filter, update, {
     new: true,
   });
-  res.contentType(contentType);
   res.status(200).json({
     status: 'success',
     doc,
