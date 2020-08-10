4585400e6022760dbbadb3f76041313834137c37
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-10 01:50:03 -0300

    Change mostPlayed function name and json send

--- controllers/quiz/getQuizzes.js
+++ controllers/quiz/getQuizzes.js
@@ -1,9 +1,9 @@
 const { quizModel } = require('../../models');
 
 module.exports = async (req, res) => {
-  const mostPlayedQuizzes = await quizModel.find().sort('timesPlayed').limit(4);
+  const mostPlayed = await quizModel.find().sort('timesPlayed').limit(4);
   res.status(200).json({
     status: 'success',
-    mostPlayedQuizzes,
+    mostPlayed,
   });
 };
