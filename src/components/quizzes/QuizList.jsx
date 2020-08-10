8b9827e20f2713c3beb579a52c7ff150235d6885
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-10 02:10:59 -0300

    Change MostPlayed component to a reusable one

--- src/components/quizzes/QuizList.jsx
+++ src/components/quizzes/QuizList.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import ButtonQuiz from './ButtonQuiz';
 
-function MostPlayed() {
-  const { mostPlayedQuizzes } = useSelector(
-    (selectorData) => selectorData.quizzesReducer.quizzes
-  );
+function QuizList({ type }) {
+  const quizData = useSelector((state) => state.quizzesReducer.quizzes[type]);
 
   return (
     <div className="Quizzes__sectiontwo">
       <span>Most played</span>
       <div>
-        {mostPlayedQuizzes.map((quiz) => (
+        {quizData.map((quiz) => (
           <ButtonQuiz key={quiz._id} quiz={quiz} />
         ))}
       </div>
@@ -19,4 +18,8 @@ function MostPlayed() {
   );
 }
 
-export default MostPlayed;
+QuizList.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default QuizList;
