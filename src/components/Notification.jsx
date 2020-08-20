a311ea9deda39b8d4526b85c3455338a253e962d
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-20 14:35:37 -0300

    Turns notification component to functional

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Notification({ type }) {
+function Notification({ callbackfunction, message }) {
   return (
-    <div className="App__notification">
-      <div className="App__notification--text">
-        <p className="App__notification--text--title">Error</p>
-        <p className="App__notification--text--desc">
-          Sorry for that, seems like we had a connection problem
-        </p>
+    <div className="Notification" onAnimationEnd={() => callbackfunction()}>
+      <div className="Notification__container">
+        <p className="Notification__error">Error</p>
+        <p className="Notification__message">{message}</p>
       </div>
     </div>
   );
 }
 
 Notification.propTypes = {
-  type: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  callbackfunction: PropTypes.func,
+};
+
+Notification.defaultProps = {
+  callbackfunction: () => {},
 };
 
 export default Notification;
