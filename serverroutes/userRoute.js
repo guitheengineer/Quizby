03aa3d58b29b39d12d68f3f017b5445de5485c74
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-23 14:17:38 -0300

    Add post router with delete and edit functions on userRoute

--- serverroutes/userRoute.js
+++ serverroutes/userRoute.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const { changePhoto, saveQuizResult } = require('../controllers/user');
-const { newQuiz, getUserQuizzes } = require('../controllers/quiz');
+const {
+  changePhoto,
+  saveQuizResult,
+  newQuiz,
+  getUserQuizzes,
+  deleteQuiz,
+  editQuiz,
+} = require('../controllers/user');
 
 router.get('/:username', getUserQuizzes);
 router.post('/changephoto', changePhoto);
 router.post('/:username/createquiz', newQuiz);
+router.post('/:username/deletequiz', deleteQuiz);
+router.post('/:username/editquiz', editQuiz);
 router.post('/savequiz', saveQuizResult);
 module.exports = router;
