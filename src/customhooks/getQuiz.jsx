8abce310b1af53c155b0708870a1c93610187381
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-23 15:05:27 -0300

    getQuiz function now receive ID from link parameter

--- src/customhooks/getQuiz.jsx
+++ src/customhooks/getQuiz.jsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { getCurrentQuiz } from '../asyncActions';
 import { selectQuiz } from '../slices/quizzesSlice';
 
 export default () => {
   const dispatch = useDispatch();
   const quiz = useSelector(selectQuiz);
+  const { id } = useParams();
 
   useEffect(() => {
-    dispatch(getCurrentQuiz());
+    dispatch(getCurrentQuiz(id));
   }, []);
 
   return quiz;
