a213445f8f4c606dc93b7edf01f5e645bc88e9d7
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-23 14:14:01 -0300

    Insert functions in the index of user controller

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -2,10 +2,18 @@ const changePhoto = require('./changePhoto');
 const checkIfUserExists = require('./checkIfUserExists');
 const checkIfEmailExists = require('./checkIfEmailExists');
 const saveQuizResult = require('./saveQuizResult');
+const getUserQuizzes = require('./getUserQuizzes');
+const newQuiz = require('./newQuiz');
+const deleteQuiz = require('./deleteQuiz');
+const editQuiz = require('./editQuiz');
 
 module.exports = {
   changePhoto,
   checkIfUserExists,
   checkIfEmailExists,
   saveQuizResult,
+  getUserQuizzes,
+  newQuiz,
+  deleteQuiz,
+  editQuiz,
 };
