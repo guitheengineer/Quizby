2a07d24144606669a9def0c042395d1eaf029cf4
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-23 15:04:51 -0300

    Add working buttonquizpermission to user page

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Clipboard, Crosshair, Coffee } from 'react-feather';
+import { useParams } from 'react-router-dom';
+import capitalize from '../../appUtils/capitalize';
 import BackgroundContainer from '../../components/backgroundcontainer';
 import { selectUserReducer } from '../../slices/userSlice';
-import { getUserQuizzes } from '../../asyncActions';
+import { getUserQuizzes, verifyUser } from '../../asyncActions';
 import { selectQuizReducer } from '../../slices/quizzesSlice';
 import ButtonQuiz from '../../components/quizzes/ButtonQuiz';
-import Stats from '../../components/user/Number';
+import Number from '../../components/user/Number';
+import ButtonQuizPermission from '../../components/user/ButtonQuizPermission';
 
 function User() {
-  const { username } = useSelector(selectUserReducer);
+  const { isAuthenticated, username } = useSelector(selectUserReducer);
+  const { username: usernameParam } = useParams();
+
   const {
     quizzesPlayed,
     quizzesCreated,
@@ -17,11 +22,32 @@ function User() {
     countQuizzesPlayed,
     countQuizzesCreated,
   } = useSelector(selectQuizReducer);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getUserQuizzes());
+    dispatch(getUserQuizzes(usernameParam));
+    const token = localStorage.getItem('TOKEN');
+    if (token) {
+      dispatch(verifyUser(token));
+    }
   }, [username]);
+
+  const chooseButton = useCallback(
+    (quiz) => {
+      if (isAuthenticated && usernameParam === username) {
+        return (
+          <ButtonQuizPermission
+            key={quiz._id}
+            quiz={quiz}
+            username={usernameParam}
+          />
+        );
+      }
+      return <ButtonQuiz key={quiz._id} className="User__item" quiz={quiz} />;
+    },
+    [usernameParam, username]
+  );
 
   return (
     <BackgroundContainer
@@ -31,15 +57,15 @@ function User() {
     >
       <div className="User">
         <section className="User__profile">
-          <h4 className="User__title">User profile</h4>
+          <h4 className="User__title">{capitalize(usernameParam)} profile</h4>
           <ul className="User__stats">
-            <Stats
+            <Number
               icon={Clipboard}
               label={countQuizzesCreated}
               description="Created"
             />
-            <Stats icon={Crosshair} label={quizAverage} description="Rating" />
-            <Stats
+            <Number icon={Crosshair} label={quizAverage} description="Rating" />
+            <Number
               icon={Coffee}
               label={countQuizzesPlayed}
               description="Games"
@@ -47,20 +73,30 @@ function User() {
           </ul>
         </section>
         <section className="User__played">
-          <h4 className="User__title">Played recently</h4>
-          <ul className="User__quizzes">
-            {quizzesPlayed.map((quiz) => (
-              <ButtonQuiz key={quiz.id} className="User__item" quiz={quiz} />
-            ))}
-          </ul>
+          {quizzesPlayed[0] && (
+            <>
+              <h4 className="User__title">Played recently</h4>
+              <ul className="User__quizzes">
+                {quizzesPlayed.map((quiz) => (
+                  <ButtonQuiz
+                    key={quiz.id}
+                    className="User__item"
+                    quiz={quiz}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
         </section>
         <section className="User__created">
-          <h4 className="User__title">Created</h4>
-          <ul className="User__quizzes">
-            {quizzesCreated.map((quiz) => (
-              <ButtonQuiz key={quiz.id} className="User__item" quiz={quiz} />
-            ))}
-          </ul>
+          {quizzesCreated[0] && (
+            <>
+              <h4 className="User__title">Created</h4>
+              <ul className="User__quizzes User__quizzes--created">
+                {quizzesCreated.map((quiz) => chooseButton(quiz))}
+              </ul>
+            </>
+          )}
         </section>
       </div>
     </BackgroundContainer>
