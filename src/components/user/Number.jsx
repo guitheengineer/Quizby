2b7a813bde00dde09f60ca702d8dff367cfecb30
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-23 15:10:09 -0300

    Stats component modifications Change Stats name to Number Decrease Icon size Change appropriated propTypes

--- src/components/user/Number.jsx
+++ src/components/user/Number.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Stats({ icon: Icon, label, description }) {
+function Number({ icon: Icon, label, description }) {
   return (
     <li className="User__numbers">
-      <Icon color="#5a5a72" size={22} />
+      <Icon color="#5a5a72" size={19} />
       <div className="User__label">{label}</div>
       <p className="User__description">{description}</p>
     </li>
   );
 }
 
-Stats.propTypes = {
-  label: PropTypes.string.isRequired,
+Number.propTypes = {
+  label: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-  icon: PropTypes.element.isRequired,
+  icon: PropTypes.object.isRequired,
 };
 
-export default Stats;
+export default Number;
