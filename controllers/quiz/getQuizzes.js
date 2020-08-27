667c8c3687b4f642e1874e70aa0c8f2b98c1b80c
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-08-27 11:28:51 -0300

    Remove console.logs

--- controllers/quiz/getQuizzes.js
+++ controllers/quiz/getQuizzes.js
@@ -1,7 +1,8 @@
 const { quizModel } = require('../../models');
 
 module.exports = async (req, res) => {
-  const mostPlayed = await quizModel.find().sort('timesPlayed').limit(4);
+  console.log(req.body);
+  const mostPlayed = await quizModel.find().sort({ timesPlayed: -1 }).limit(4);
   res.status(200).json({
     status: 'success',
     mostPlayed,
