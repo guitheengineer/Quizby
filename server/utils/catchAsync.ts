d33f25f0f9a2e2db1cbd2035e40e6c52c7c09fd4
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-10-18 00:13:50 -0300

    feat: fix catchAsync return type not specified

--- server/utils/catchAsync.ts
+++ server/utils/catchAsync.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 
 export default (
   fn: (req: Request, res: Response, next: NextFunction) => any
-) => (req: Request, res: Response, next: NextFunction) => {
+) => (req: Request, res: Response, next: NextFunction): void => {
   fn(req, res, next).catch(next);
 };
