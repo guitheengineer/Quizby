704f273831a9260cd4b9c06ae72a48ecce6dd3ec
Guilherme Nunes <guilhermesnd14@gmail.com>
2020-10-18 00:29:01 -0300

    feat: add asyncActions of slices

---
+++ src/slices/manipulate-slice/async-actions.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { simpleFetch, postFetch } from 'utils';
+import { QuizData, QuizForm } from '../../types';
+
+const reducer = 'manipulateReducer';
+
+export const sendForm = createAsyncThunk(
+  `${reducer}/sendForm`,
+  async (quizData: QuizForm) => postFetch('user/:id/createquiz', quizData)
+);
+
+export const getQuizEdit = createAsyncThunk(
+  `${reducer}/getQuizEdit`,
+  async (id: string) => simpleFetch(`quizzes/quiz/${id}`)
+);
+
+export const editQuizThunk = createAsyncThunk(
+  `${reducer}/editQuizThunk`,
+  async (quizData: QuizData) =>
+    postFetch(`user/${quizData.username}/editquiz`, quizData)
+);
---
+++ src/slices/user-slice/async-actions.ts
@@ -0,0 +1,20 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { postFetch, simpleFetch } from 'utils';
+import { ModifyQuiz } from '../../types';
+
+const reducer = 'userReducer';
+
+export const saveQuizResult = createAsyncThunk(
+  `${reducer}/savequiz`,
+  async ({
+    percentage,
+    quizId,
+    username,
+  }: Pick<ModifyQuiz, 'percentage' | 'quizId' | 'username'>) =>
+    postFetch(`user/${username}/savequiz`, { percentage, quizId })
+);
+
+export const verifyUser = createAsyncThunk(
+  `${reducer}/verifyUser`,
+  async (token: string) => simpleFetch('verifyuser', token)
+);
