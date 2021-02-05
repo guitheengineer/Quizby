29fd656b6e197adb58a4af44bb7278c76b43d57a
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:58:12 -0300

    feat: add playing styles

--- src/pages/playing/Playing.tsx
+++ src/pages/playing/Playing.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import AnswerAnimation from './components/AnswerAnimation';
 import Game from './components/Game';
 import useQuiz from '../hooks/useQuiz';
+import './playing.scss';
+import { useAppSelector } from '../../store';
+import { selectQuizReducer } from '../../slices/quizzes-slice';
 
 const Playing = () => {
   useQuiz();
-
+  const { currentQuestionAnswered } = useAppSelector(selectQuizReducer);
   return (
     <>
       <Game />
-      <AnswerAnimation />
+      {currentQuestionAnswered ? <AnswerAnimation /> : null}
     </>
   );
 };
