4c30ff5fad5fe5479df5d7160709ae75edae4cc5
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:39:51 -0300

    fix: classnames following cascading priority

--- src/pages/quizzes/components/QuizList.tsx
+++ src/pages/quizzes/components/QuizList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useAppSelector } from 'store';
-import { QuizClient } from 'types';
+import { useAppSelector } from '../../../store';
+import { QuizClient } from '../../../types';
 import ButtonQuiz from './ButtonQuiz';
 
 type Props = {
@@ -9,15 +9,18 @@ type Props = {
   className?: string;
 };
 
-const QuizList = ({ type, label, className }: Props) => {
+const QuizList = ({ type, label, className = '' }: Props) => {
   const quizData = useAppSelector((state) => state.quizzes.quizzes[type]);
-
   return (
-    <div className={`Quizzes__sectiontwo ${className}`}>
+    <div className={`Quizzes__list ${className}`}>
       <span>{label}</span>
-      <div>
+      <div className="Quizzes__list-container">
         {quizData.map((quiz: QuizClient) => (
-          <ButtonQuiz key={quiz._id} quiz={quiz} />
+          <ButtonQuiz
+            titleClassName="Quizzes__title"
+            key={quiz._id}
+            quiz={quiz}
+          />
         ))}
       </div>
     </div>
