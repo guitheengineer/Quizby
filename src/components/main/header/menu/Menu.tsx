29d29141b9e7ff66f1c168fc34f73fc275f95808
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 15:12:13 -0300

    feat: add functionality to menu

--- src/components/main/header/menu/Menu.tsx
+++ src/components/main/header/menu/Menu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faUser,
@@ -6,25 +7,43 @@ import {
   faChartLine,
 } from '@fortawesome/free-solid-svg-icons';
 import SearchQuizzes from '../../../common/search-quizzes/SearchQuizzes';
+import { Link, useHistory } from 'react-router-dom';
 
-const Menu = () => (
-  <div className="Header__menu">
-    <SearchQuizzes />
-    <ul>
-      <li>
-        <FontAwesomeIcon icon={faUser} />
-        Profile
-      </li>
-      <li>
-        <FontAwesomeIcon icon={faChartLine} />
-        Statistics
-      </li>
-      <li>
-        <FontAwesomeIcon icon={faSignOutAlt} />
-        Quit
-      </li>
-    </ul>
-  </div>
-);
+const Menu = () => {
+  const history = useHistory();
+  const handleQuit = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <div className="Header__menu">
+      <SearchQuizzes iconClassName="Search-quizzes__icon--menu" />
+      <div className="Header__list">
+        <Link
+          className="Header__item"
+          to={`/user/${localStorage.getItem('USERNAME')}`}
+        >
+          <li>
+            <FontAwesomeIcon className="Header__svg" icon={faUser} />
+            Profile
+          </li>
+        </Link>
+        <Link className="Header__item" to="">
+          <li>
+            <FontAwesomeIcon className="Header__svg" icon={faChartLine} />
+            Statistics
+          </li>
+        </Link>
+        <Link className="Header__item" to="">
+          <li onClick={handleQuit}>
+            <FontAwesomeIcon className="Header__svg" icon={faSignOutAlt} />
+            Quit
+          </li>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
