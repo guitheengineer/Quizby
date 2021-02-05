4de3b17d563263f5aeeaed9c6a282d37542afaa3
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 13:52:08 -0300

    fix: presentation BEM rules

--- src/components/common/presentation/Presentation.tsx
+++ src/components/common/presentation/Presentation.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './presentation.scss';
 
-type Props = { title: string; description: string; marginTop: string };
+type Props = { title: string; description: string; marginTop?: string };
 
 const Presentation = ({ title, description, marginTop = '0px' }: Props) => {
   return (
-    <div style={{ marginTop }} className="App__presentation">
-      <p className="App__presentation--title">{title}</p>
-      <p className="App__presentation--description">{description}</p>
+    <div style={{ marginTop }} className="Presentation">
+      <p className="Presentation__title">{title}</p>
+      <p className="Presentation__description">{description}</p>
     </div>
   );
 };
