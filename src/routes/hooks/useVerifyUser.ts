7e0fb27d2b1a41f981402fec06ffc3b8d31cad29
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:05:14 -0300

    fix: remove unused checkauth state and token parameter

--- src/routes/hooks/useVerifyUser.ts
+++ src/routes/hooks/useVerifyUser.ts
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from 'store';
-import { verifyUser } from 'slices/user-slice/async-actions';
-import { selectUserReducer } from 'slices/user-slice/user-slice';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { verifyUser } from '../../slices/user-slice/async-actions';
+import { selectUserReducer } from '../../slices/user-slice/user-slice';
 
-export default () => {
+const useVerifyUser = () => {
   const dispatch = useAppDispatch();
-  const { isAuthenticated, checkAuth, username } = useAppSelector(
-    selectUserReducer
-  );
+  const { isAuthenticated, username } = useAppSelector(selectUserReducer);
   useEffect(() => {
     const token = localStorage.getItem('TOKEN');
     if (token) {
-      dispatch(verifyUser(token));
+      dispatch(verifyUser());
     }
   }, []);
 
-  return { isAuthenticated, checkAuth, username };
+  return { isAuthenticated, username };
 };
+
+export default useVerifyUser;
