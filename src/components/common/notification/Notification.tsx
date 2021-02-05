c95aa1ebf68f5ae669fae53088e64a8a46555cfc
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 13:15:15 -0300

    feat: update notification component

--- src/components/common/notification/Notification.tsx
+++ src/components/common/notification/Notification.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import './notification.scss';
+
 import { AsyncThunk } from '@reduxjs/toolkit';
 import { QuizForm as QuizFormType } from 'types';
 import QuizForm from '../quiz-form';
 
 type Props = {
   message?: string;
-  callback: any;
 };
 
 const Notification = ({
-  callback,
   message = 'An error has ocurred, try again later',
 }: Props) => {
   return (
-    <div className="Notification" onAnimationEnd={() => callback()}>
+    <div className="Notification">
       <div className="Notification__container">
         <p className="Notification__error">Error</p>
         <p className="Notification__message">{message}</p>
