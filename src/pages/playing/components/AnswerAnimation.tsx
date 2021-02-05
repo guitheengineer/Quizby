37da3ed55ed55f885fcdc98d9e529bed2276ae12
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:57:42 -0300

    feat: add answer animation styles

--- src/pages/playing/components/AnswerAnimation.tsx
+++ src/pages/playing/components/AnswerAnimation.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useAppSelector } from 'store';
-import { selectQuizReducer } from 'slices/quizzes-slice';
+import { useAppSelector } from '../../../store';
+import { selectQuizReducer } from '../../../slices/quizzes-slice';
 import { useHistory } from 'react-router-dom';
-import correct from 'assets/icons/correct.svg';
-import wrong from 'assets/icons/wrong.svg';
+import correct from '../../../assets/icons/correct.svg';
+import wrong from '../../../assets/icons/wrong.svg';
+import './answer-animation.scss';
 
 const AnswerAnimation = () => {
   const { userAnsweredCorrect, userStats, currentQuiz } = useAppSelector(
@@ -23,14 +24,14 @@ const AnswerAnimation = () => {
     <img
       onAnimationEnd={animationEnd}
       src={correct}
-      className="App__playing--correct"
+      className="Answer-animation"
       alt="Correct answer"
     />
   ) : (
     <img
       onAnimationEnd={animationEnd}
       src={wrong}
-      className="App__playing--correct"
+      className="Answer-animation"
       alt="Wrong answer"
     />
   );
