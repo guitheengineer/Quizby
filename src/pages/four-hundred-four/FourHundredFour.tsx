eb4ab88956d13e605286a8ff2cfe44d5e6fc1946
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 15:02:31 -0300

    feat: add 404 page styles

--- src/pages/four-hundred-four/FourHundredFour.tsx
+++ src/pages/four-hundred-four/FourHundredFour.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import './four-hundred-four.scss';
 import { Link } from 'react-router-dom';
 import fourZeroFour from 'assets/error-images/404.png';
 import BackgroundContainer from '../../components/main/background-container';
 
 const FourHundredFour = () => (
   <BackgroundContainer>
-    <div className="App__fourhundred">
-      <div className="App__fourhundred--illustration">
+    <div className="fourhundred">
+      <div className="fourhundred--illustration">
         <img alt="" src={fourZeroFour} />
         404
       </div>
-      <p className="App__fourhundred--message">This page doesn&apos;t exists</p>
+      <p className="fourhundred--message">This page doesn&apos;t exists</p>
       <Link to="/">
-        <button type="button" className="App__fourhundred--button">
+        <button type="button" className="fourhundred--button">
           Go home
         </button>
       </Link>
