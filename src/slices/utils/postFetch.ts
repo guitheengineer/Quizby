6eeb3a4a059d66cc4c7e2a92fd2668ee7f32978b
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 13:53:59 -0300

    refactor: add authorization for post requests

--- src/slices/utils/postFetch.ts
+++ src/slices/utils/postFetch.ts
@@ -1,8 +1,10 @@
 const postFetch = async (linkApi: string, body: any) => {
+  const token = localStorage.getItem('TOKEN');
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(body),
   };
