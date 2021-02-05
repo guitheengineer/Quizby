d2c9b462622a2d63c5d989e312cbab4460402ec3
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 15:05:58 -0300

    feat: add background-container styles and loading handler

--- src/components/main/background-container/BackgroundContainer.tsx
+++ src/components/main/background-container/BackgroundContainer.tsx
@@ -1,10 +1,19 @@
-import React, { Children, cloneElement, CSSProperties } from 'react';
+import React, { Children, cloneElement, CSSProperties, useEffect } from 'react';
+import './background-container.scss';
 import Menu from '../header/menu';
-import { selectGeneralReducer } from '../../../slices/general-slice/general-slice';
+import {
+  changeMenu,
+  selectGeneralReducer,
+} from '../../../slices/general-slice/general-slice';
 import { useAppSelector } from '../../../store/store';
+import { ThunkResponses } from '../../../types';
+import { useDispatch } from 'react-redux';
 
 type Props = CSSProperties & {
+  isLoading?: ThunkResponses;
   children: React.ReactElement;
+  className?: string;
+  fetch?: any;
 };
 
 const BackgroundContainer = ({
@@ -13,19 +22,25 @@ const BackgroundContainer = ({
   top = '0px',
   alignItems = 'normal',
   justifyContent = 'center',
-  height = '6rem',
-  minHeight,
   overflow = 'hidden',
+  paddingBottom = 0,
+  isLoading = 'fulfilled',
+  minHeight,
+  className,
 }: Props) => {
   const { menuIsActive } = useAppSelector(selectGeneralReducer);
-
+  const dispatch = useDispatch();
   const commonStyles: CSSProperties = {
     marginTop,
     top,
     alignItems,
     justifyContent,
-    height,
+    height: menuIsActive ? '6rem' : null,
+    paddingBottom,
   };
+  useEffect(() => {
+    dispatch(changeMenu(false));
+  }, []);
 
   const inactiveMenu: CSSProperties = {
     ...commonStyles,
@@ -34,15 +49,14 @@ const BackgroundContainer = ({
   const activeMenu: CSSProperties = {
     ...commonStyles,
     overflow,
-    minHeight,
     flex: '1 1 100%',
+    minHeight,
   };
-
-  return (
+  return isLoading === 'fulfilled' || isLoading === 'pending' ? (
     <>
       {menuIsActive && <Menu />}
       <div
-        className="Background-container"
+        className={`Background-container ${className}`}
         style={menuIsActive ? inactiveMenu : activeMenu}
       >
         {Children.map(children, (child) =>
@@ -54,7 +68,7 @@ const BackgroundContainer = ({
         )}
       </div>
     </>
-  );
+  ) : null;
 };
 
 export default BackgroundContainer;
