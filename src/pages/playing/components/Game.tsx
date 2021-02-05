58b4edae30d911353dd1a6206e42d1f3de33c9c1
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:56:38 -0300

    fix: game classname

--- src/pages/playing/components/Game.tsx
+++ src/pages/playing/components/Game.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import FetchError from 'components/common/fetch-error/FetchError';
-import LoaderSpinner from 'components/common/loader-spinner/LoaderSpinner';
-import BackgroundContainer from 'components/main/background-container';
-import { getCurrentQuiz } from 'slices/quizzes-slice/async-actions';
-import { useAppSelector } from 'store';
-import { selectQuizReducer } from 'slices/quizzes-slice';
-import ListAnswers from './ListIconAnswers';
+import FetchError from '../../../components/common/fetch-error/FetchError';
+import LoaderSpinner from '../../../components/common/loader-spinner/LoaderSpinner';
+import BackgroundContainer from '../../../components/main/background-container';
+import { getCurrentQuiz } from '../../../slices/quizzes-slice/async-actions';
+import { useAppSelector } from '../../../store';
+import { selectQuizReducer } from '../../../slices/quizzes-slice';
+import IconAnswers from './IconAnswers';
 import Question from './Question';
 import Answers from './Answers';
 
@@ -13,19 +13,19 @@ const Game = () => {
   const { quizFetchState, currentQuestionAnswered } = useAppSelector(
     selectQuizReducer
   );
-
   return (
     <BackgroundContainer>
-      <div className={`App__playing ${currentQuestionAnswered && 'fade'}`}>
+      <div className={`Playing ${currentQuestionAnswered && 'fade'}`}>
         <LoaderSpinner loadingState={quizFetchState} />
-        {quizFetchState === 'fetched' && (
+        {quizFetchState === 'fulfilled' ? (
           <>
-            <ListAnswers />
+            <IconAnswers />
             <Question />
             <Answers />
           </>
-        )}
-        {quizFetchState === 'error' && <FetchError fetch={getCurrentQuiz} />}
+        ) : quizFetchState === 'rejected' ? (
+          <FetchError fetch={getCurrentQuiz} />
+        ) : null}
       </div>
     </BackgroundContainer>
   );
