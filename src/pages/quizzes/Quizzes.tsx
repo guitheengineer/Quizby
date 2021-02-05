95b7dd865bc262a6286a75c8759d8c37c451ccc2
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:41:08 -0300

    fix: quizzes error handling

--- src/pages/quizzes/Quizzes.tsx
+++ src/pages/quizzes/Quizzes.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
-import { ClipLoader } from 'react-spinners';
-import BackgroundContainer from 'components/main/background-container';
-import FetchError from 'components/common/fetch-error/FetchError';
-import SearchQuizzes from 'components/common/search-quizzes/SearchQuizzes';
-import { selectQuizReducer } from 'slices/quizzes-slice';
+import './quizzes.scss';
+import BackgroundContainer from '../../components/main/background-container';
+import FetchError from '../../components/common/fetch-error/FetchError';
+import SearchQuizzes from '../../components/common/search-quizzes/SearchQuizzes';
+import { selectQuizReducer } from '../../slices/quizzes-slice';
 import {
   quizzesAdded,
   searchQuizzes,
-} from 'slices/quizzes-slice/async-actions';
-import { useAppSelector, useAppDispatch } from 'store';
+} from '../../slices/quizzes-slice/async-actions';
+import { useAppSelector, useAppDispatch } from '../../store';
 import { Location } from 'history';
-import Categories from './components/Categories';
+import { Categories } from '../../components/common/categories';
 import QuizList from './components/QuizList';
 import QuizzesSearched from './components/QuizzesSearched';
 import Recommended from './components/Recommended';
@@ -22,8 +22,9 @@ type Props = {
 const Quizzes = ({ location }: Props) => {
   const dispatch = useAppDispatch();
   const { quizzesFetchState, query } = useAppSelector(selectQuizReducer);
+
   useEffect(() => {
-    if (query === '') {
+    if (!query) {
       dispatch(quizzesAdded());
     } else {
       dispatch(searchQuizzes(location.search.substring(3)));
@@ -36,31 +37,24 @@ const Quizzes = ({ location }: Props) => {
       alignItems="center"
       overflow="visible"
     >
-      <>
-        <ClipLoader
-          css={`
-            position: absolute;
-            top: 46%;
-            margin: auto;
-          `}
-          loading={quizzesFetchState === 'loading' && query === ''}
-          color="#5255CA"
+      <div className="Quizzes">
+        <SearchQuizzes
+          className="Quizzes__search"
+          inputClassName="Quizzes__input"
         />
-        <div className="Quizzes__container">
-          <SearchQuizzes />
-          {quizzesFetchState === 'fulfilled' && (
+        {query ? (
+          <QuizzesSearched />
+        ) : (
+          quizzesFetchState === 'fulfilled' && (
             <>
               <Recommended />
               <QuizList label="Most played" type="mostPlayed" />
               <Categories />
             </>
-          )}
-          {quizzesFetchState === 'rejected' && (
-            <FetchError fetch={quizzesAdded} />
-          )}
-          {query !== '' && <QuizzesSearched />}
-        </div>
-      </>
+          )
+        )}
+        <FetchError fetchState={quizzesFetchState} fetch={quizzesAdded} />
+      </div>
     </BackgroundContainer>
   );
 };
