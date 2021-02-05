d2d801e179e00c1a3fc8d107735592beae4b6051
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-05 14:34:51 -0300

    fix: recommended styles and unused input maxlength of 40

--- src/pages/quizzes/components/Recommended.tsx
+++ src/pages/quizzes/components/Recommended.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useAppSelector } from 'store';
+import { useAppSelector } from '../../../store';
 import ButtonQuiz from './ButtonQuiz';
 
 const Recommended = () => {
-  const recommended = useAppSelector(
-    (state) => state.quizzes.quizzes.recommended
-  );
+  const {
+    quizzes: { recommended },
+  } = useAppSelector((state) => state.quizzes);
   return (
     <div className="Quizzes__recommended">
       <span>Recommended</span>
-      <ButtonQuiz maxLength={40} quiz={recommended} />
+      <div className="Quizzes__recommended-wrapper">
+        <ButtonQuiz quiz={recommended} />
+      </div>
     </div>
   );
 };
