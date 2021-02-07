debf28aba739d7cce3ce239fc2bbe3100c822199
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-07 00:00:06 -0300

    feat: when clicking around menu it closes

--- src/pages/landing/components/LandingMenu.tsx
+++ src/pages/landing/components/LandingMenu.tsx
@@ -2,24 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../components/landing-menu.scss';
 
-const LandingMenu = () => {
-  {
-    /*to-do: if user clicks on anything thats is not a list item, close menu */
-  }
-
-  return (
-    <div onClick={() => console.log('click')} className="landing-menu">
-      <ul>
-        <Link to={`/user/${localStorage.getItem('USERNAME')}`}>
-          <li className="landing-menu__item">Profile</li>
-        </Link>
-
-        <li className="landing-menu__item">Signup</li>
+const LandingMenu = ({ setMenuOpen }: { setMenuOpen: any }) => (
+  <div
+    onClick={(e) => {
+      if (e.target === e.currentTarget) {
+        setMenuOpen(false);
+      }
+    }}
+    className="landing-menu"
+  >
+    <ul>
+      <Link to={`/user/${localStorage.getItem('USERNAME')}`}>
         <li className="landing-menu__item">Profile</li>
-        <li className="landing-menu__item">Login</li>
-      </ul>
-    </div>
-  );
-};
+      </Link>
+
+      <li className="landing-menu__item">Signup</li>
+      <li className="landing-menu__item">Profile</li>
+      <li className="landing-menu__item">Login</li>
+    </ul>
+  </div>
+);
 
 export default LandingMenu;
