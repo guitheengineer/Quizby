b097450efcabd8381fe127bac027bc91a2edbef5
gsdeveloper <gsdevelopercontact@gmail.com>
2021-02-28 14:53:04 -0300

    fix: fetch-error wrong return and styles

--- src/components/common/fetch-error/FetchError.tsx
+++ src/components/common/fetch-error/FetchError.tsx
@@ -12,6 +12,7 @@ type Props = {
   message?: string;
   btnMessage?: string;
   children?: ReactNode;
+  className?: string;
 };
 
 const FetchError = ({
@@ -20,34 +21,29 @@ const FetchError = ({
   type = 'fetch',
   btnMessage = 'Try again',
   children = 'Sorry, an error ocurred',
+  className = '',
 }: Props) => {
   const dispatch = useAppDispatch();
   const history = useHistory();
-  return (
-    fetchState === 'rejected' && (
-      <div className="Fetch-error">
-        <img
-          className="Fetch-error__illustration"
-          alt="error"
-          src={fetchError}
-        />
-        <p className="Fetch-error__message">{children}</p>
-        <button
-          type="button"
-          onClick={() => {
-            if (type === 'fetch') {
-              dispatch(fetch());
-            } else {
-              history.push('/');
-            }
-          }}
-          className="Fetch-error__button"
-        >
-          {btnMessage}
-        </button>
-      </div>
-    )
-  );
+  return fetchState === 'rejected' ? (
+    <div className={`Fetch-error ${className}`}>
+      <img className="Fetch-error__illustration" alt="error" src={fetchError} />
+      <p className="Fetch-error__message">{children}</p>
+      <button
+        type="button"
+        onClick={() => {
+          if (type === 'fetch') {
+            dispatch(fetch());
+          } else {
+            history.push('/');
+          }
+        }}
+        className="Fetch-error__button"
+      >
+        {btnMessage}
+      </button>
+    </div>
+  ) : null;
 };
 
 export default FetchError;
