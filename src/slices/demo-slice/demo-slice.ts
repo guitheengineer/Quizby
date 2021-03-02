a0c02f7fae0e469a016f45b56d1ddcb53311d4f8
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-01 22:18:30 -0300

    fix: rootReducer import

--- src/slices/demo-slice/demo-slice.ts
+++ src/slices/demo-slice/demo-slice.ts
@@ -1,4 +1,4 @@
-import { RootState } from './../../store/store';
+import { RootState } from './../../store/rootReducer';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface SliceState {
