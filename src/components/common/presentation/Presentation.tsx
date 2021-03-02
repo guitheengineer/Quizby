b9803be89c938dd61ef03e35a08427ebf1932dab
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-02 16:31:33 -0300

    refactor: reduce code in presentation

--- src/components/common/presentation/Presentation.tsx
+++ src/components/common/presentation/Presentation.tsx
@@ -5,17 +5,15 @@ import './presentation.scss';
 
 type Props = { title: string; description: string; marginTop?: string };
 
-const Presentation = ({ title, description, marginTop }: Props) => {
-  return (
-    <>
-      <LandingMenu />
-      <div style={{ marginTop }} className="Presentation">
-        <p className="Presentation__title">{title}</p>
-        <p className="Presentation__description">{description}</p>
-        <LandingIconMenu className="Presentation__menu" />
-      </div>
-    </>
-  );
-};
+const Presentation = ({ title, description, marginTop }: Props) => (
+  <>
+    <LandingMenu />
+    <div style={{ marginTop }} className="Presentation">
+      <p className="Presentation__title">{title}</p>
+      <p className="Presentation__description">{description}</p>
+      <LandingIconMenu className="Presentation__menu" />
+    </div>
+  </>
+);
 
 export default Presentation;
