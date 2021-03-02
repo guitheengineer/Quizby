545472df8ecc4075d73cab67a0831e41cf5e02d1
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-02 09:17:29 -0300

    feat: add landing menu to signup and login

--- src/components/common/presentation/Presentation.tsx
+++ src/components/common/presentation/Presentation.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import LandingIconMenu from 'pages/landing/components/LandingIconMenu';
+import LandingMenu from 'pages/landing/components/LandingMenu';
 import './presentation.scss';
 
 type Props = { title: string; description: string; marginTop?: string };
 
-const Presentation = ({ title, description, marginTop = '0px' }: Props) => {
+const Presentation = ({ title, description, marginTop }: Props) => {
   return (
-    <div style={{ marginTop }} className="Presentation">
-      <p className="Presentation__title">{title}</p>
-      <p className="Presentation__description">{description}</p>
-    </div>
+    <>
+      <LandingMenu />
+      <div style={{ marginTop }} className="Presentation">
+        <p className="Presentation__title">{title}</p>
+        <p className="Presentation__description">{description}</p>
+        <LandingIconMenu className="Presentation__menu" />
+      </div>
+    </>
   );
 };
 
