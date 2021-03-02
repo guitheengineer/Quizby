479bc6570f5b5a7bb61d92e479757a7d7165f870
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-02 16:33:15 -0300

    feat: change error link to quizzes instead of landing page

--- src/pages/four-hundred-four/FourHundredFour.tsx
+++ src/pages/four-hundred-four/FourHundredFour.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './four-hundred-four.scss';
 import { Link } from 'react-router-dom';
 import fourZeroFour from 'assets/error-images/404.png';
-import BackgroundContainer from '../../components/main/background-container';
+import BackgroundContainer from 'components/main/background-container';
 
 const FourHundredFour = () => (
   <BackgroundContainer>
@@ -12,7 +12,7 @@ const FourHundredFour = () => (
         404
       </div>
       <p className="fourhundred--message">This page doesn&apos;t exists</p>
-      <Link to="/">
+      <Link to="/quizzes">
         <button type="button" className="fourhundred--button">
           Go home
         </button>
