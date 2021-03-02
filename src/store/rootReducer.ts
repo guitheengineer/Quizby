68e835df55fe22c92136d747a3d0b302f7deaccf
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-01 22:13:30 -0300

    refactor: rootReducer to clean imports using absolute path

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,9 @@
-import form from '../slices/form-slice/form-slice';
-import quizzes from '../slices/quizzes-slice';
-import user from '../slices/user-slice/user-slice';
-import general from '../slices/general-slice/general-slice';
-import manipulate from '../slices/manipulate-slice';
-import demo from '../slices/demo-slice';
+import form from 'slices/form-slice/form-slice';
+import quizzes from 'slices/quizzes-slice';
+import user from 'slices/user-slice/user-slice';
+import general from 'slices/general-slice/general-slice';
+import manipulate from 'slices/manipulate-slice';
+import demo from 'slices/demo-slice';
 import { combineReducers } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
