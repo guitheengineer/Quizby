7eac1c4a5a53b73a1366cbe4763a237b9b257ac4
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-02 15:21:30 -0300

    refactor: solve usequiz useeffect dependencies

--- src/components/hooks/useQuiz.tsx
+++ src/components/hooks/useQuiz.tsx
@@ -1,20 +1,22 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getCurrentQuiz } from '../../slices/quizzes-slice/async-actions';
-import { selectQuiz } from '../../slices/quizzes-slice';
-import { useAppDispatch, useAppSelector } from '../../store';
+import { getCurrentQuiz } from 'slices/quizzes-slice/async-actions';
+import { selectQuiz } from 'slices/quizzes-slice';
+import { useAppDispatch, useAppSelector } from 'store';
 
 interface ParamTypes {
   id: string;
 }
-export default () => {
+const useQuiz = () => {
   const dispatch = useAppDispatch();
   const quiz = useAppSelector(selectQuiz);
   const { id } = useParams<ParamTypes>();
 
   useEffect(() => {
     dispatch(getCurrentQuiz(id));
-  }, []);
+  }, [dispatch, id]);
 
   return quiz;
 };
+
+export default useQuiz;
