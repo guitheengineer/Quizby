2ed621cb22d5add77274e4ebd05d258327d2cab2
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-01 22:54:15 -0300

    feat: handle user existance

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useCallback } from 'react';
 import './user.scss';
 import { Clipboard, Crosshair, Coffee } from 'react-feather';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import BackgroundContainer from '../../components/main/background-container';
 import { useAppSelector, useAppDispatch } from '../../store';
 import { selectQuizReducer, getUserQuizzes } from '../../slices/quizzes-slice';
@@ -39,11 +39,14 @@ const User = () => {
 
   useEffect(() => {
     dispatch(checkIfUserExists(usernameParam));
-  }, [usernameParam]);
+  }, [usernameParam, dispatch]);
 
   useEffect(() => {
-    dispatch(getUserQuizzes(usernameParam));
-  }, [username]);
+    // if user exists, display his quizzes
+    if (usernameState === 'fulfilled' && errorExistsUsername.errorExists)
+      dispatch(getUserQuizzes(usernameParam));
+  }, [dispatch, usernameParam, usernameState, errorExistsUsername.errorExists]);
+
   const chooseButton = useCallback(
     (quiz) => {
       if (isAuthenticated && usernameParam === username) {
@@ -57,7 +60,7 @@ const User = () => {
       }
       return <ButtonQuiz key={quiz._id} className="User__item" quiz={quiz} />;
     },
-    [usernameParam, username]
+    [usernameParam, username, isAuthenticated]
   );
 
   return (
@@ -69,14 +72,38 @@ const User = () => {
         justifyContent="normal"
         alignItems="center"
         overflow="normal"
+        isLoading={[usernameState, getUserQuizzesState, deleteQuizFetchState]}
       >
-        <div className="User">
-          {errorExistsUsername.errorExists ? (
+        {errorExistsUsername.errorExists ? (
+          <div
+            className="User"
+            style={{
+              marginBottom:
+                isAuthenticated && usernameParam === username ? '8rem' : 0,
+            }}
+          >
             <>
               <section className="User__profile">
-                <h4 className="User__title User__title--username">
-                  {usernameParam}
-                </h4>
+                <div className="User__info">
+                  <h4
+                    className="User__title User__title--username"
+                    style={{
+                      textAlign:
+                        isAuthenticated && usernameParam === username
+                          ? 'initial'
+                          : 'center',
+                    }}
+                  >
+                    @{usernameParam}
+                  </h4>
+                  {isAuthenticated && usernameParam === username && (
+                    <button className="User__create-quiz" type="button">
+                      <Link to={`/user/${username}/createquiz`}>
+                        Create quiz
+                      </Link>
+                    </button>
+                  )}
+                </div>
                 <ul className="User__stats">
                   <Number
                     icon={Clipboard}
@@ -111,25 +138,39 @@ const User = () => {
                   </>
                 ) : null}
               </section>
-              <section className="User__created">
+              <section
+                className={`User__created ${
+                  quizzesPlayed.length ? 'User__created--second' : null
+                }`}
+              >
                 {quizzesCreated.length ? (
                   <>
                     <h4 className="User__title">Created</h4>
-                    <ul className="User__quizzes User__quizzes--created">
+                    <ul
+                      className={`User__quizzes ${
+                        isAuthenticated && usernameParam === username
+                          ? 'User__quizzes--created'
+                          : null
+                      }`}
+                    >
                       {quizzesCreated.map((quiz: any) => chooseButton(quiz))}
                     </ul>
                   </>
                 ) : null}
               </section>
             </>
-          ) : (
-            usernameState === 'fulfilled' && (
-              <FetchError type="common" btnMessage="Go home">
-                User not found
-              </FetchError>
-            )
-          )}
-        </div>
+          </div>
+        ) : usernameState === 'fulfilled' ? (
+          <FetchError
+            className="User__error"
+            type="common"
+            btnMessage="Go home"
+          >
+            User not found
+          </FetchError>
+        ) : (
+          <></>
+        )}
       </BackgroundContainer>
     </>
   );
