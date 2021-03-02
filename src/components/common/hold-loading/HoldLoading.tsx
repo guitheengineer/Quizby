0255ec0bb7bf9f7946d247ccbfac236dfa6d08a0
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-01 22:10:21 -0300

    refactor: remove unused code from holdloading component

--- src/components/common/hold-loading/HoldLoading.tsx
+++ src/components/common/hold-loading/HoldLoading.tsx
@@ -7,16 +7,12 @@ type Props = {
   isLoading: ThunkResponses | ThunkResponses[];
 };
 
-const HoldLoading = ({ isLoading }: Props) => {
-  // Check if any of elements given is pending to display loading.
-
-  return (Array.isArray(isLoading) &&
-    isLoading.some((el) => el === 'pending')) ||
-    isLoading === 'pending' ? (
+const HoldLoading = ({ isLoading }: Props) =>
+  (Array.isArray(isLoading) && isLoading.some((el) => el === 'pending')) ||
+  isLoading === 'pending' ? (
     <div className="hold-loading">
       <ClipLoader loading color="white" size={35} />
     </div>
   ) : null;
-};
 
 export default HoldLoading;
