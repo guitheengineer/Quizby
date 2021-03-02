7246c4701da772a6d8a12b3cc335515575aac437
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-01 23:13:52 -0300

    refactor: remove unused imports from login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,18 +1,10 @@
-import React, { SyntheticEvent } from 'react';
-import {
-  onSubmitForm,
-  selectFormReducer,
-} from '../../slices/form-slice/form-slice';
-import { useAppSelector, useAppDispatch } from '../../store';
-import { regexEmailValidator } from '../../utils';
-import { postLogin } from '../../slices/form-slice/async-actions';
+import React from 'react';
 import Presentation from '../../components/common/presentation';
 import LoginContainer from './components/LoginContainer';
 
 const Login = () => (
   <div className="Sign__container">
     <Presentation
-      marginTop="3.4rem"
       title="Login"
       description="Enter to start creating and sharing fun quizzes to your friends!"
     />
