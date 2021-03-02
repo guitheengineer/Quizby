88ebe29887fca0a724b78fa41d8b7e3c0230aff8
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-02 15:22:19 -0300

    feat: add menu options

--- src/components/main/header/menu/Menu.tsx
+++ src/components/main/header/menu/Menu.tsx
@@ -1,46 +1,95 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import '../header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faUser,
   faSignOutAlt,
-  faChartLine,
+  faPlusSquare,
+  faGamepad,
+  faUserPlus,
+  faSignInAlt,
+  IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
-import SearchQuizzes from '../../../common/search-quizzes/SearchQuizzes';
-import { Link, useHistory } from 'react-router-dom';
+import SearchQuizzes from 'components/common/search-quizzes/SearchQuizzes';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { changeMenu } from 'slices/general-slice';
+import { useAppDispatch } from 'store';
+import { resetUser } from 'slices/user-slice';
+
+const MenuItem = ({
+  link,
+  icon,
+  children,
+}: {
+  link: string;
+  icon: IconDefinition;
+  children: ReactNode;
+}) => (
+  <Link className="Header__item" to={link}>
+    <FontAwesomeIcon className="Header__svg" icon={icon} />
+    <span className="Header__item-title">{children}</span>
+  </Link>
+);
+
+const MenuItemQuizzes = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <Link
+      onClick={() => {
+        dispatch(changeMenu({ type: 'menuIsActive', isActive: false }));
+      }}
+      className="Header__item"
+      to="/quizzes"
+    >
+      <FontAwesomeIcon className="Header__svg" icon={faGamepad} />
+      <span className="Header__item-title">Quizzes</span>
+    </Link>
+  );
+};
 
 const Menu = () => {
-  const history = useHistory();
-  const handleQuit = () => {
-    localStorage.clear();
-    history.push('/');
-  };
-
+  const username = localStorage.getItem('USERNAME');
+  const dispatch = useAppDispatch();
   return (
     <div className="Header__menu">
       <SearchQuizzes iconClassName="Search-quizzes__icon--menu" />
       <div className="Header__list">
-        <Link
-          className="Header__item"
-          to={`/user/${localStorage.getItem('USERNAME')}`}
-        >
-          <li>
-            <FontAwesomeIcon className="Header__svg" icon={faUser} />
-            Profile
-          </li>
-        </Link>
-        <Link className="Header__item" to="">
-          <li>
-            <FontAwesomeIcon className="Header__svg" icon={faChartLine} />
-            Statistics
-          </li>
-        </Link>
-        <Link className="Header__item" to="">
-          <li onClick={handleQuit}>
-            <FontAwesomeIcon className="Header__svg" icon={faSignOutAlt} />
-            Quit
-          </li>
-        </Link>
+        {username ? (
+          <>
+            <MenuItem icon={faUser} link={`/user/${username}`}>
+              Profile
+            </MenuItem>
+            <MenuItemQuizzes />
+            <MenuItem icon={faPlusSquare} link={`/user/${username}/createquiz`}>
+              Create quiz
+            </MenuItem>
+            <Link
+              to="/"
+              onClick={() => {
+                localStorage.removeItem('TOKEN');
+                localStorage.removeItem('USER');
+                localStorage.removeItem('USERNAME');
+                dispatch(resetUser());
+              }}
+              className="Header__item"
+            >
+              <FontAwesomeIcon className="Header__svg" icon={faSignOutAlt} />
+              <span className="Header__item-title">Quit</span>
+            </Link>
+          </>
+        ) : (
+          <>
+            <MenuItem icon={faUserPlus} link="/signup">
+              Signup
+            </MenuItem>
+            <MenuItem icon={faSignInAlt} link="/login">
+              Login
+            </MenuItem>
+            <MenuItemQuizzes />
+          </>
+        )}
       </div>
     </div>
   );
