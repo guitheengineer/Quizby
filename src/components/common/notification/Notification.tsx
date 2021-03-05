9ea7ac9cfe45209d9e1167fcbdbb2926ccdaf194
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-05 16:40:19 -0300

    feat: add close button to notification

--- src/components/common/notification/Notification.tsx
+++ src/components/common/notification/Notification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './notification.scss';
 
 type Props = {
@@ -8,14 +8,21 @@ type Props = {
 const Notification = ({
   message = 'An error has ocurred, try again later',
 }: Props) => {
-  return (
-    <div className="Notification">
-      <div className="Notification__container">
-        <p className="Notification__error">Error</p>
-        <p className="Notification__message">{message}</p>
+  const [isClosed, setIsClosed] = useState(false);
+  return !isClosed ? (
+    <div className="notification">
+      <div className="notification__container">
+        <p className="notification__error">Error</p>
+        <p className="notification__message">{message}</p>
+        <button
+          onClick={() => setIsClosed(true)}
+          className="notification__close"
+        >
+          X
+        </button>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Notification;
