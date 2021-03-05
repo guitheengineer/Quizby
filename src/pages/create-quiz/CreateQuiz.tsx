16a946dc000dacef88b47673f50774083f8e7b57
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-05 16:43:29 -0300

    fix: reset all fields and user enter create quiz

--- src/pages/create-quiz/CreateQuiz.tsx
+++ src/pages/create-quiz/CreateQuiz.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import QuizForm from 'components/common/quiz-form';
-import { selectManipulateReducer } from 'slices/manipulate-slice';
+import {
+  resetQuizInfo,
+  selectManipulateReducer,
+} from 'slices/manipulate-slice';
 import { sendForm } from 'slices/manipulate-slice/async-actions';
 import { useAppSelector } from 'store';
+import { useDispatch } from 'react-redux';
 
 const CreateQuiz = () => {
   const { saveQuizFetchState } = useAppSelector(selectManipulateReducer);
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(resetQuizInfo());
+  }, [dispatch]);
   return (
     <QuizForm
       functionType={sendForm}
