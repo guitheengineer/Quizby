f82972a120dc14532d28aff2222f9fb5651da61e
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-05 17:29:55 -0300

    fix: showing empty quizzes when search result is empty

--- src/pages/quizzes/components/QuizList.tsx
+++ src/pages/quizzes/components/QuizList.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useAppSelector } from 'store';
 import { QuizClient } from 'types';
 import ButtonQuiz from './ButtonQuiz';
 
 type Props = {
-  type: 'mostPlayed' | 'quizzesSearchedData' | 'recommended' | 'category';
+  type: 'mostPlayed' | 'quizzesSearchedData' | 'category';
   label?: string;
   className?: string;
 };
@@ -12,19 +12,36 @@ type Props = {
 const QuizList = ({ type, label, className = '' }: Props) => {
   const quizData = useAppSelector((state) => state.quizzes.quizzes[type]);
 
+  const chooseFetchState = useCallback(() => {
+    switch (type) {
+      case 'category':
+        return 'categoryFetchState';
+
+      case 'mostPlayed':
+      case 'quizzesSearchedData':
+        return 'quizFetchState';
+    }
+  }, [type]);
+
+  const fetchState = useAppSelector(
+    (state) => state.quizzes[chooseFetchState()]
+  );
+
   return (
     <div className={`Quizzes__list ${className}`}>
       <span>{label}</span>
       <div className="Quizzes__list-container">
-        {quizData.length > 0
-          ? quizData.map((quiz: QuizClient) => (
-              <ButtonQuiz
-                titleClassName="Quizzes__title"
-                key={quiz._id}
-                quiz={quiz}
-              />
-            ))
-          : [...Array(4)].map((_, i) => <ButtonQuiz key={i} />)}
+        {fetchState === 'fulfilled' &&
+          quizData.length > 0 &&
+          quizData.map((quiz: QuizClient) => (
+            <ButtonQuiz
+              titleClassName="Quizzes__title"
+              key={quiz._id}
+              quiz={quiz}
+            />
+          ))}
+        {fetchState === 'pending' &&
+          [...Array(4)].map((_, i) => <ButtonQuiz key={i} />)}
       </div>
     </div>
   );
