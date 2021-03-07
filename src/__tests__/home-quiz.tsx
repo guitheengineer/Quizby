ac8e77be670a09a128b1c6fb1ab59207be6849e4
gsdeveloper <gsdevelopercontact@gmail.com>
2021-03-07 15:33:39 -0300

    test: demo-quiz on landing page

---
+++ src/__tests__/home-quiz.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { render } from 'utils';
+import { QuizDemo } from 'pages/landing/components';
+
+test('if home quiz correct answer works correctly', async () => {
+  render(<QuizDemo />);
+  userEvent.click(screen.getByText('Barack Obama'));
+
+  await screen.findByText('100');
+  await screen.findByText('Perfect score!');
+});
+
+test('if home quiz wrong answer works correctly', async () => {
+  render(<QuizDemo />);
+  userEvent.click(screen.getByText('James Warren'));
+
+  await screen.findByText('0');
+  await screen.findByText('Incorrect :(');
+});
